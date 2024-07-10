4fe6b0b8f37c803d6e7cc8ddc9896b315a95f315
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-07-11 00:53:55 +0530

    changed

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,15 @@
+ export const signup = async(req,res)=>{
+    res.json({
+        mssg: "you have hit the signup end point"
+    })
+}
+export const logout = async(req,res)=>{
+    res.json({
+        mssg: "you have hit the signup end point"
+    })
+}
+export const login = async(req,res)=>{
+    res.json({
+        mssg: "you have hit the signup end point"
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
-
-
+import authRoutes from './routes/auth.routes.js'
 const app = express();
+
+app.use('/api/auth',authRoutes );
+
 
 app.listen(8000,()=>{
    console.log(`server running on the port ${8000}`)
