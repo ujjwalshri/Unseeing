d302aa0e8781ce0d7a74332dee7248059cb68732
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-07-13 00:00:33 +0530

    db connected

---
+++ backend/db/mongo.connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connetMongoDB =  async() =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`mongo db connected ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`connections errror  ${error}`);
+        process.exit(1);
+    }
+}
+
+export default connetMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import authRoutes from './routes/auth.routes.js'
+import dotenv from 'dotenv';
+import connetMongoDB from "./db/mongo.connection.js";
 const app = express();
-
+dotenv.config();
 app.use('/api/auth',authRoutes );
 
-
-app.listen(8000,()=>{
-   console.log(`server running on the port ${8000}`)
+console.log(process.env.MONGO_URI);
+const port = process.env.PORT || 8000
+app.listen(port,()=>{
+   console.log(`server running on the port ${port}`)
+   connetMongoDB();
 })
 
