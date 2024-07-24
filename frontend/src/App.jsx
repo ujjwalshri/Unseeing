2f0299f0c08fb94717274479aa5c9a76e25fa797
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-07-25 00:28:15 +0530

    SETUP UI/ DESIGN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
+import RightPanel from "./components/svgs/common/RightPanel"
+import Sidebar from "./components/svgs/common/SideBar"
 import About from "./pages/about/About"
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import HomePage from "./pages/home/HomePage"
 import { Routes, Route } from "react-router-dom"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 function App() {
 
   return (
     <>
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
     <Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
         <Route path='/about' element={<About />} />
+        <Route path='/notifications' element={<NotificationPage/>} />
+        <Route path='/profile/:id' element={<ProfilePage/>} />
 			</Routes>
+      <RightPanel/>
     </div>
     </>
   )
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -2,19 +2,31 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import XSvg from "../../../components/svgs/X";
-
+import { FaGraduationCap } from "react-icons/fa6";
 import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
+import { FaBook } from "react-icons/fa";
 import { MdDriveFileRenameOutline } from "react-icons/md";
 
 const SignUpPage = () => {
+    const[notification, setNotification] = useState(false);
 
+	const [branch, setBranch] = useState("select your branch");
+	useEffect(() => {	
+		setTimeout(() => {		
+			setNotification(true);
+		}, 3000);	
+		setInterval(() => {
+			setNotification(false);
+		}	, 10000);
+	}, []);
+	
 	const [formData, setFormData] = useState({
 		email: "",
-		username: "",
-		fullName: "",
 		password: "",
+		stream : "", // Added stream to formData
+		branch: "", // Added branch to formData
 	});
 
 	const handleSubmit = (e) => {
@@ -23,33 +35,46 @@ const SignUpPage = () => {
 	};
 
 	const handleInputChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleBranchChange = (branch) => {
+		setFormData({ ...formData, branch });
+		setBranch(branch);
 	};
 
 	const isError = false;
 
 	return (
+        
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-            <div>
-             
-            </div>
-			<div  className='flex-1 hidden lg:flex items-center  justify-center'>
-            <h1 className="text-5xl font-bold">Welcome to Unseeing!</h1>
-            
-            <div>
-           
-           <img src="../public/Unseeing logo.png" alt="" />
-           <div className="flex justify-center items-center mt-2">
+			<div>
+  {notification && (
+    <div>
+<div className="toast bg">
+  <div className="alert alert-info bg-gray-700">
+    <span className="font-xl text-white font-mono font-extrabold"> Attention!! we do not save/share your details to anyone</span>
+  </div>
 
-           <h1>What is unseeing ??? </h1>
-           <Link to='/about' className="btn  font-mono font-extrabold">About us</Link>
-           </div>
-           
-       </div>
-            </div>
+</div>
+    </div>
+  )}
+</div>
+			<div></div>
+			<div className='flex-1 hidden lg:flex items-center justify-center'>
+				<h1 className="text-5xl font-bold">Welcome to Unseeing!</h1>
+				<div>
+					<img src="../public/Unseeing logo.png" alt="" />
+					<div className="flex justify-center items-center mt-2">
+						<h1>What is unseeing ??? </h1>
+						<Link to='/about' className="btn font-mono font-extrabold">About us</Link>
+					</div>
+				</div>
+			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
-				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
+				<form className='lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
+					<img src="../public/Unseeing logo.png" alt="" />
 					<h1 className='text-4xl font-extrabold text-white'>Join today.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
@@ -62,30 +87,34 @@ const SignUpPage = () => {
 							value={formData.email}
 						/>
 					</label>
-					<div className='flex gap-4 flex-wrap'>
-						<label className='input input-bordered rounded flex items-center gap-2 flex-1'>
-							<FaUser />
-							<input
-								type='text'
-								className='grow '
-								placeholder='Branch'
-								name='username'
-								onChange={handleInputChange}
-								value={formData.username}
-							/>
-						</label>
-						<label className='input input-bordered rounded flex items-center gap-2 flex-1'>
-							<MdDriveFileRenameOutline />
-							<input
-								type='text'
-								className='grow'
-								placeholder='Stream'
-								name='fullName'
-								onChange={handleInputChange}
-								value={formData.fullName}
-							/>
-						</label>
-					</div>
+					
+						<div className="dropdown flex-1 flex flex-col justify-center items-center ">
+							<div tabIndex={0} role="button" name="branch" className="btn m-1 lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col">{branch}</div>
+							<ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+								<li><button type="button" onClick={() => handleBranchChange("B.tech") }>B.tech</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("IT")}>IT</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("BBA")}>BBA</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("LLB")}>LLB</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("BCA")}>BCA</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("BA")}>BA</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("MA")}>MA</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("MBA")}>MBA</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("BCOM")}>BCOM</button></li>
+								<li><button type="button" onClick={() => handleBranchChange("MCOM")}>MCOM</button></li>
+							</ul>
+						</div>
+						<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdPassword />
+						<input
+							type='text'
+							className='grow'
+							placeholder='Stream / type NA if not applicable'
+							name='stream'
+							onChange={handleInputChange}
+							value={formData.stream}
+						/>
+					</label>
+					
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdPassword />
 						<input
@@ -110,4 +139,5 @@ const SignUpPage = () => {
 		</div>
 	);
 };
+
 export default SignUpPage;
